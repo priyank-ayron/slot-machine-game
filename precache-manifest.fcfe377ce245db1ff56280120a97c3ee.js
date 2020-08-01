528ad49026c9c1f9202c35b83cb4166a033bce22
Priyank <93072pri@gmail.com>
2020-08-01 21:32:08 +0530

    Updates

---
+++ precache-manifest.fcfe377ce245db1ff56280120a97c3ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57aa4d7aaf7bb12990338890fc1d2975",
+    "url": "/slot-machine-game/index.html"
+  },
+  {
+    "revision": "7fbe7f0c83dff6be90d5",
+    "url": "/slot-machine-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4af3a4bb6ebf0eada1b3",
+    "url": "/slot-machine-game/static/js/2.ff19b380.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slot-machine-game/static/js/2.ff19b380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fbe7f0c83dff6be90d5",
+    "url": "/slot-machine-game/static/js/main.474337b7.chunk.js"
+  },
+  {
+    "revision": "a8965788bbaffca765fc",
+    "url": "/slot-machine-game/static/js/runtime-main.32b2f065.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/slot-machine-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
