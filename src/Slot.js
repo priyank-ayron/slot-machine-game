2b43661b875369e2c41cb50d3fa8a0441d1fde3b
Priyank <93072pri@gmail.com>
2020-08-02 11:16:08 +0530

    array destructuring

--- src/Slot.js
+++ src/Slot.js
@@ -3,9 +3,7 @@ import "./index.css";
 import Condition from "./Condition";
 
 const Slot = (props) => {
-  let x = props.x;
-  let y = props.y;
-  let z = props.z;
+  let { x, y, z } = props;
   return x === y && y === z ? (
     <Condition a={x} b={y} c={z} text="This is matching"></Condition>
   ) : (
